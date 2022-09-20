5d637bb9c6660b492aac1b81a16dc35c8bfc6da9
wassim <wassimbenjdida@gmail.com>
2022-09-20 20:55:58 +0100

    done: del images from S3

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const {
   S3Client,
   GetObjectCommand,
   ListObjectsCommand,
+  DeleteObjectCommand,
 } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const getS3Image = require("./utils/getS3Image");
@@ -14,6 +15,13 @@ const multer = require("multer");
 const multerS3 = require("multer-s3");
 
 const upload = multer({
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      cb(null, true);
+    } else {
+      cb("Invalid file type, only JPEG and PNG is allowed!", false);
+    }
+  },
   storage: multerS3({
     s3: S3,
     bucket: process.env.AWS_S3_BUCKET,
@@ -32,9 +40,18 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/", async (req, res) => {
-  const url = await getS3Image(S3, "win_bg.jpg");
+  const objs = await S3.send(
+    new ListObjectsCommand({ Bucket: process.env.AWS_S3_BUCKET })
+  );
+
+  const urls = [];
+  for (const obj of objs.Contents) {
+    const url = await getS3Image(S3, obj.Key);
+    urls.push({ url: url, key: obj.Key });
+  }
+
   res.render("index", {
-    img: url,
+    images: urls,
   });
 });
 
@@ -43,10 +60,20 @@ app.get("/upload", async (req, res) => {
 });
 
 app.post("/upload", upload.single("image"), async (req, res, next) => {
-  // res.render("uploaded: \n");
-  console.log("Uploaded: ");
-  console.log(req.file);
-  next();
+  res.redirect("/upload");
+});
+
+app.get("/delete/:key", async (req, res) => {
+  const delRes = await S3.send(
+    new DeleteObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: req.params.key,
+    })
+  );
+
+  console.log(delRes);
+
+  res.redirect("/");
 });
 
 const PORT = 9999;
